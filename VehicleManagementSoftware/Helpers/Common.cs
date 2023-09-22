33510afb6b8d409467408b081af8e1c1e480b97e
huongntq <22810039@student.hcmute.edu.vn>
2023-09-22 19:01:15 +0700

    Check branch commit

--- VehicleManagementSoftware/Helpers/Common.cs
+++ VehicleManagementSoftware/Helpers/Common.cs
@@ -47,5 +47,6 @@ namespace VehicleManagementSoftware.Helpers
         {
             Console.WriteLine("+-----------+----+----------+----------------+-------------+--------+---------+------------+-------------+-------------+");
         }
+        //Check branch commit
     }
 }
